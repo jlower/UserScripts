b6a7edec3d8639910a1c7fae2174c8e1d84a3888
lowo <870050851@qq.com>
2024-11-10 16:54:48 +0800

    js change

--- thisav或missav永远播放/missavPlayForever.user.js
+++ thisav或missav永远播放/missavPlayForever.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name thisav或missav永远播放
+// @description thisav或missav永远播放！重写了播放器的pause方法，只有在页面具有焦点时才执行原始的暂停操作。更改match里的内容适配更多网站。
 // @description:en Remove lose focus -> stop playing + Remove first click pop-up advert
-// @description thisav或missav永远播放!
 
 // @namespace https://www.lowoneko.eu.org/
 // @author lowo
 // @license AGPL-3.0
-// @version 1.0.3
+// @version 1.0.4
 
 // @match        https://missav.com/*
 // @match        https://thisav.com/*
@@ -17,27 +17,21 @@
 
 (function () {
     'use strict';
-
     // 当页面加载完成后执行以下代码
     document.addEventListener('ready', () => {
         console.log('ready'); // 输出“ready”到控制台，用于调试
-
         // 禁止使用window.open函数，防止页面打开新窗口
         window.open = () => { };
-
         // 获取播放器的原始暂停方法，保存到变量pause中
         const pause = window.player.pause;
-
         // 重写播放器的pause方法
         window.player.pause = () => {
             console.log('pasu'); // 输出“pasu”到控制台，用于调试
-
             // 检查页面是否具有焦点，只有在页面具有焦点时才执行原始的暂停操作
             if (document.hasFocus()) {
                 pause(); // 调用原始的pause方法，暂停播放器
             }
         };
     });
-
     // Your code here...
 })();
