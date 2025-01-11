12e528531dc0db188c3e1c0c69dd673634688c1f
lowo <870050851@qq.com>
2025-01-12 01:43:39 +0800

    Update missavPlayForever.user.js

--- thisav或missav永远播放/missavPlayForever.user.js
+++ thisav或missav永远播放/missavPlayForever.user.js
@@ -9,6 +9,7 @@
 // @version 1.0.4
 
 // @match        https://missav.com/*
+// @match        https://missav.ws/*
 // @match        https://thisav.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=missav.com
 // @grant        none
